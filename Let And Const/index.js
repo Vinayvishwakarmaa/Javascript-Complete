44538771033390e414806f612753bbf053813391
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-25 17:06:17 +0530

    Let and Const

---
+++ Let And Const/index.js
@@ -0,0 +1,24 @@
+
+// console.log(this)//{}
+
+const accountId = 143444;
+let accountEmaill = "vinay@google.com";
+var accountPassword = "vinay123";
+accountCity = "Gwalior";
+let accountState;
+
+ //accountId = 2;//Assignment to constant variable. //Not allowed
+ //accountEmaill = "Vinay@amazone.com" // replace
+// accountPassword = "3344" // replace
+// accountCity ="Delhi" // Replace
+// console.log(accountEmaill);
+
+
+/****   
+  Prefer not to use var
+  because of issue in block scope and functional scope
+  ****/
+
+
+console.table([accountId,accountEmaill, accountPassword, accountCity,accountState]);
+
