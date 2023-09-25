d5c2eeabdc125bbf5b94abce6d412870a5a04acb
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-25 17:35:33 +0530

    Datatypes and ECMA standards

---
+++ Datatypes and ECMA standards/dataTypes.js
@@ -0,0 +1,23 @@
+
+"use strict "; // treat all JS code as newer version
+
+// alert(3 + 3); //popup
+
+// console.log(3 + 3); // code readability should be high
+
+let name = "Vinay";
+let age = 21;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not define
+// symbol
+
+// Object
+
+console.log(typeof null)//Object
+console.log(typeof undefined) // undefined
