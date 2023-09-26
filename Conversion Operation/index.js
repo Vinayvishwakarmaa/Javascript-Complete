356bc45b7ec19a5e1e8e119128b362328b323cf0
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-26 07:03:35 +0530

    Conversion Operation

---
+++ Conversion Operation/index.js
@@ -0,0 +1,26 @@
+let score = true;
+// let score = "33abc";
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33;
+// "3abc" => NaN
+// true => 1; false =>0;
+
+let isLoggedIn = "Vinay";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false;
+// "Vinay" => true
+
+
+// let NumebrSo = 63;
+// let stringNumber = String(NumebrSo);
+// console.log(typeof stringNumber);
+
