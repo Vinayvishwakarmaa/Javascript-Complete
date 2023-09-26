8262e3f399baabf64e1e31a6e9f9764a990f4492
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-26 07:31:22 +0530

    Comparision

--- Conversion Operation/index.js
+++ Conversion Operation/index.js
@@ -24,3 +24,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // let stringNumber = String(NumebrSo);
 // console.log(typeof stringNumber);
 
+/****************  Operations  ******************/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Vinay";
+let str2 = " Vishwakarma";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(2*2-2+1/4);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 4 + 4;
+// console.log(num1, num2, num3);
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
