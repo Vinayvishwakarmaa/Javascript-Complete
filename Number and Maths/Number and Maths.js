2bddae6b387092e3e634f5b08e451ce361a71a00
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-27 05:58:38 +0530

    Math.random() * (max - min + 1)

---
+++ Number and Maths/Number and Maths.js
@@ -0,0 +1,29 @@
+// const score = 456;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8956;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++ Mats ++++++++++++++++/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.8));
+// console.log(Math.ceil(3.8));
+// console.log(Math.min(3,45,35,6,2,5,89));
+// console.log(Math.max(3, 45, 35, 6, 2, 5, 89));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
