cdf915fa753bc9fd1af2c47b6a05a40cc5ca4d58
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-09-29 12:32:40 +0530

    Interations

---
+++ Control Flow/switch.js
@@ -0,0 +1,57 @@
+
+// switch (key) {
+//       case value:
+            
+//             break;
+
+//       default:
+//             break;
+// }
+
+
+// const month = 13;
+
+// switch (month) {
+//       case 1:
+//          console.log("January");
+//             break;
+//       case 2:
+//          console.log("Feb");
+//             break;
+//       case 3:
+//          console.log("Mar");
+//             break;
+//       case 4:
+//          console.log("April");
+//             break;
+//       case 5:
+//          console.log("May");
+//             break;
+//       case 6:
+//          console.log("June");
+//             break;
+//       case 7:
+//          console.log("July");
+//             break;
+//       case 8:
+//          console.log("Aguest");
+//             break;
+//       case 9:
+//          console.log("Septmber");
+//             break;
+//       case 10:
+//          console.log("October");
+//             break;
+//       case 11:
+//          console.log("Novber");
+//             break;
+//       case 12:
+//          console.log("Dec");
+//             break;
+
+//       default:
+//        console.log("default case match");
+//             break;
+// }
+
+
